9a213ff11d16a072d8c31daec0f083860f975421
tylermneher <tylermneher@users.noreply.github.com>
2022-11-13 13:11:26 +0000

    deploy: 9356ec7e328fb89ceb9408785c4a8befbca798c3

---
+++ component---src-pages-404-jsx-fc9c1ef0bac7d3dd2271.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjodie=self.webpackChunkjodie||[]).push([[256],{7291:function(e,t,l){l.r(t);var n=l(7294),u=l(7197);t.default=()=>n.createElement(u.Z,null,n.createElement("div",{style:{textAlign:"center"}},n.createElement("h1",null,"404"),n.createElement("p",null,"Page not found.")))}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-fc9c1ef0bac7d3dd2271.js.map
