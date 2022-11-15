fa21a7cfdf48e7786d5b616684cb39b6f5105c1b
Tyler M. Neher <tyler@neherdata.com>
2022-11-15 03:27:56 -0500

    fixed size of logo

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 const Logo = () => (
-  <img src={"https://github.com/neherdata.png"}></img>
+  <img src={"https://github.com/neherdata.png?size=70"}></img>
   );
 
 export default Logo
