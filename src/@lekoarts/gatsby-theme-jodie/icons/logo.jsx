21e1375d41d62f36d0c0cea0860c1c41d474955a
Tyler M. Neher <tyler@neherdata.com>
2022-11-15 03:15:46 -0500

    fixed logo syntax

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 const Logo = () => (
-  <img src={"https://github.com/neherdata.png"}></img>;
-)
+  <img src={"https://github.com/neherdata.png"}></img>
+  );
 
 export default Logo
